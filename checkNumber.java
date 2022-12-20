cd7310ae5b00e9ba0cbf75e27c3ac9b820445c02
Aruna vattikota <108143232+Aruna-vattikota@users.noreply.github.com>
2022-12-20 18:24:42 +0530

    Add files via upload

---
+++ checkNumber.java
@@ -0,0 +1,16 @@
+package welcome;
+
+public class checkNumber {
+	public static void main(String[] args) {
+		int num = 75;
+		
+		if (num > 0)
+			System.out.println(num + " is POSITIVE NUMBER.");
+		else if (num < 0)
+			System.out.println(num + " is NEGTIVE NUMBER.");
+		else
+			System.out.println(num + "is a ZERO.");
+
+	}
+
+}
