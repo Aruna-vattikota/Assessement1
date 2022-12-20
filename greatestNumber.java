55e826be02f30b72b419ce700d5f046c8f32cfe9
Aruna vattikota <108143232+Aruna-vattikota@users.noreply.github.com>
2022-12-20 18:33:17 +0530

    Add files via upload

---
+++ greatestNumber.java
@@ -0,0 +1,14 @@
+package welcome;
+
+public class greatestNumber {
+	public static void main(String[] args) {
+		int a = 40, b = 78, c = 19;
+		if (a >= b && a >= c)
+			System.out.println(a + "is the lagest NUMBER");
+		else if (b >= a && b >= c)
+			System.out.println(b + "is the lagest NUMBER");
+		else
+			System.out.println(c + "is the largest NUMBER");
+	}
+
+}
