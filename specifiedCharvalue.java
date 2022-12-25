4628b99d0c632b66caf51aa8be7557ef104e6fbb
Aruna vattikota <108143232+Aruna-vattikota@users.noreply.github.com>
2022-12-25 21:35:00 +0530

    Add files via upload

---
+++ specifiedCharvalue.java
@@ -0,0 +1,15 @@
+package welcome;
+
+public class specifiedCharvalue {
+	public static void main(String[] args) {
+        String str1 = "welcome to";
+        String str2 = "carrertuner";
+        System.out.println("Original String: " + str1);
+        System.out.println("Specified sequence of char values: " + str2);
+        System.out.println(str1.contains(str2));
+    }
+}
+
+	
+
+
