1ffc70b26326e97a0015169e32e7abb69b1a5f3e
Aruna vattikota <108143232+Aruna-vattikota@users.noreply.github.com>
2022-12-25 21:34:43 +0530

    Add files via upload

---
+++ smallLettertoCapital.java
@@ -0,0 +1,14 @@
+package welcome;
+
+public class smallLettertoCapital {
+	public static void main(String[] args) {
+		
+		      String str = "b.v.raju college ";
+		      String strUpper = str.toUpperCase();
+		      System.out.println("Original String: " + str);
+		      System.out.println("String changed to upper case: " + strUpper);
+		   }
+		
+	}
+
+	
