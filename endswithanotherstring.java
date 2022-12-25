efc11ae2b94f3744ac605683aca22992711b20f6
Aruna vattikota <108143232+Aruna-vattikota@users.noreply.github.com>
2022-12-25 21:33:50 +0530

    Add files via upload

---
+++ endswithanotherstring.java
@@ -0,0 +1,17 @@
+package welcome;
+
+public class endswithanotherstring {
+	public static void main(String[] args) {
+		String str1 = "string Exercises";
+		String str2 = "java Exercise";
+
+		String end_str = "se";
+
+		boolean ends1 = str1.endsWith(end_str);
+		boolean ends2 = str2.endsWith(end_str);
+
+		System.out.println("\"" + str1 + "\" ends with " + "\"" + end_str + "\"? " + ends1);
+		System.out.println("\"" + str2 + "\" ends with " + "\"" + end_str + "\"? " + ends2);
+	}
+
+}
