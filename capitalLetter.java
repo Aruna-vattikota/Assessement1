ee33938cec3ffd37cdf219bc27cfb87b00d6d339
Aruna vattikota <108143232+Aruna-vattikota@users.noreply.github.com>
2022-12-25 21:33:01 +0530

    Add files via upload

---
+++ capitalLetter.java
@@ -0,0 +1,15 @@
+package welcome;
+
+public class capitalLetter {
+	public static void main(String[] args) {
+		String str = "Welcome To Carrertuner ";
+	      for(int i = 0; i < str.length(); i++) {
+	         if(Character.isUpperCase(str.charAt(i))) {
+	             System.out.println(str.charAt(i));
+	         }
+	       }
+	   }
+
+	}
+
+
