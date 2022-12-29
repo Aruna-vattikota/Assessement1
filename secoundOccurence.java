bb57dd1bb3a7d7ab0e1ec437db19ed3662af415e
Aruna vattikota <108143232+Aruna-vattikota@users.noreply.github.com>
2022-12-29 12:34:31 +0530

    Add files via upload

---
+++ secoundOccurence.java
@@ -0,0 +1,26 @@
+package welcome;
+
+public class secoundOccurence {
+	public static void main(String[] args) {
+		 
+
+			      
+		 char ch1, ch2, ch3, ch4;
+			      
+			      ch1 = 'l';
+			      ch2 = 'l';
+
+			      
+			      ch3 = Character.toUpperCase(ch1);
+			      ch4 = Character.toUpperCase(ch2);
+
+			      String str1 = "Uppercase of " + ch1 + " is " + ch3;
+			      String str2 = "Uppercase of " + ch2 + " is " + ch4;
+
+			      // print ch3, ch4 values
+			      System.out.println( str1 );
+			      System.out.println( str2 );
+
+	}
+
+}
